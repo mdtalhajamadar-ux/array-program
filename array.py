744f4482d59b109118ec103fe5974c22dd2a1182
Mohammadtalha-babasab-jamadar <mdtalhajamadar@gmail.com>
2025-11-18 12:13:01 +0530

    Update array.py

---
+++ array.py
@@ -0,0 +1,11 @@
+import sys
+if len(sys.argv) ee 2:
+script_name - sys.argv[e]
+# initialize array with at least 11 elements so index 10 is valid
+array - [None] * 11
+array[10] - sys.argv[1]else：
+array - [1, 2, 3, 9, 7,10, 5]
+sum of_elenents - sum(array)
+print("Sum of elenents:”, sum_of_elements)
+avg_of_elements · sum_of_elenents / len(array)
+print("Average of elements:”, avg_of_elements)
